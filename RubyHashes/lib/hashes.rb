50d805053dd2072111d8d7f54a93b032914792e8
Andrew Draper <adraper3@uncc.edu>
2020-01-26 20:46:41 +0000

    RubyHashes - Part 2

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -19,7 +19,22 @@ end
 
 # Part II
 def array2d_2_hash contact_info, contacts
-    # YOUR CODE HERE
+    result = {}
+    count = 0
+    
+    contacts.each do |x, y|
+        # x = key, y = value
+        # a = name,  b = email
+        if contact_info[0].empty?
+            result[x.to_sym] = {}
+        else
+            result[x.to_sym] = {email: contact_info[count][0], phone: contact_info[count][1]}
+        end
+        
+        count += 1
+    end
+    
+    return result
 end
 
 # Part III
