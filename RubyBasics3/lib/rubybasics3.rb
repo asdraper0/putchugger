ead4465adc4da843f75fd7f303ec5cf1102c1c5a
Andrew Draper <adraper3@uncc.edu>
2020-02-15 19:44:32 +0000

    Ruby OO Basics - Part 2

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -3,6 +3,9 @@
 class BookInStock
     attr_accessor :isbn, :price
     def initialize(isbn, price)
+        raise ArgumentError, 'ISBN cannot be blank' unless isbn != ''
+        raise ArgumentError, 'Price cannot be less than or equal to zero' unless price > 0
+        
         @isbn = isbn
         @price = price
     end
