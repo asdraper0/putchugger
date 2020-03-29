9c670f35bc1c673d29ece72c2164744730c8059c
Andrew Draper <adraper3@uncc.edu>
2020-03-29 19:34:42 +0000

    Adding user functionality

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -11,6 +11,7 @@ class ArticlesController < ApplicationController
     
     def create
         @article = Article.new(article_params)
+        @article.user = current_user
         #byebug
         
         if @article.save
